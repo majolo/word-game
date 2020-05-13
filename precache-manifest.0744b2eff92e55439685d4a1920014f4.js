04fd7fe114f4d1f5ea314fe730eba9a7060b07ec
Max Lowther <maxlowther@improbable.io>
2020-05-13 18:35:10 +0100

    Updates

---
+++ precache-manifest.0744b2eff92e55439685d4a1920014f4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "22164e792ad37c2a1cb1d8c6a1943877",
+    "url": "/word-game/index.html"
+  },
+  {
+    "revision": "aad62ec40068afcf77a9",
+    "url": "/word-game/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "4b61b0eca0ad4040357d",
+    "url": "/word-game/static/js/2.2bcfb2e4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/word-game/static/js/2.2bcfb2e4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aad62ec40068afcf77a9",
+    "url": "/word-game/static/js/main.f3110edf.chunk.js"
+  },
+  {
+    "revision": "aafb593b70090a32de96",
+    "url": "/word-game/static/js/runtime-main.653295a9.js"
+  }
+]);
